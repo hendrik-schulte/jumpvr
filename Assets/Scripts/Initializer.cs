c2505964948a4c19cbaf6d9c4638cc823ea9ad8b
Hendrik Schulte <dinah93@gmail.com>
2016-12-14 21:48:35 +0100

    Environment Islands + Clouds

---
+++ Assets/Scripts/Initializer.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class Initializer : MonoBehaviour
+{
+    [SerializeField]
+    private SphereCollider Collider;
+
+    [SerializeField]
+    private Transform Prefab;
+
+    [SerializeField]
+    [Range(0, 200)]
+    private int Amount;
+
+    [SerializeField]
+    [Range(0, 30)]
+    private float MinDistance;
+
+    [SerializeField]
+    [Range(0, 10)]
+    private float MinScale = 0.9f;
+
+    [SerializeField]
+    [Range(0, 10)]
+    private float MaxScale = 1.1f;
+
+    void Awake()
+    {
+        InsideCircle(Amount);
+    }
+
+    private void InsideCircle(int Amount)
+    {
+        var i = 0;
+        var Spread = Collider.radius * transform.lossyScale.x;
+
+        if (Spread < MinDistance) return;
+
+
+        while (i < Amount)
+        {
+            var randomPos = Vector3.zero;
+
+            while (randomPos.magnitude < MinDistance)
+            {
+                randomPos = transform.position + Random.insideUnitSphere * Random.value * Spread;
+            }
+
+            //            var randomAngle = Random.Range(0, 360f);
+            //            var randomScale = ;
+
+            var instance = Instantiate(Prefab, randomPos, Quaternion.AngleAxis(Random.Range(0, 360f), Vector3.up), transform);
+
+            instance.localScale = new Vector3(Random.Range(MinScale, MaxScale), Random.Range(MinScale, MaxScale), Random.Range(MinScale, MaxScale));
+
+            i++;
+        }
+    }
+}
---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    [Range(0, 10)]
+    public float Speed;
+
+    void Awake()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        StartCoroutine(UpdateAll());
+    }
+
+
+    IEnumerator UpdateAll()
+    {
+        while (true)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + Time.deltaTime * Speed, transform.rotation.eulerAngles.z);
+
+            yield return null;
+        }
+    }
+}
